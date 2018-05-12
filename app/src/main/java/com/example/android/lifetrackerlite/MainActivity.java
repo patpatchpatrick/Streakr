26160d3cb5674d8beabd0e61d6fbd15e3e142cab
Patrick Doyle <patrickdoyle30@gmail.com>
2018-05-12 15:12:33 -0500

    Add intent to goals habits feature

--- app/src/main/java/com/example/android/lifetrackerlite/MainActivity.java
+++ app/src/main/java/com/example/android/lifetrackerlite/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.lifetrackerlite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Open Goals/Habits Activity if Goals/Habits view is clicked
+        LinearLayout goalsFeatureView = (LinearLayout) findViewById(R.id.goals_feature);
+        goalsFeatureView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GoalsHabitsFeatureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

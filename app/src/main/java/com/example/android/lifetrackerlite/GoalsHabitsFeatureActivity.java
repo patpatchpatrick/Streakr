12110bc05b351ce47d10717d0b0adcc726b8aa5a
Patrick Doyle <patrickdoyle30@gmail.com>
2018-05-12 14:48:33 -0500

    Begin goals habits activity

---
+++ app/src/main/java/com/example/android/lifetrackerlite/GoalsHabitsFeatureActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.lifetrackerlite;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class GoalsHabitsFeatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals_habits_feature);
+
+    }
+
+}

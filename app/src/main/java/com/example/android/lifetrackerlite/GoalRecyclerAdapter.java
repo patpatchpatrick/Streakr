e5340e520f951e41f5bc69ab06d7d51abc68b7c5
Patrick Doyle <patrickdoyle30@gmail.com>
2018-06-06 12:03:54 -0700

    Replace listView with recyclerView and replace listAdapter with recyclerAdapter

---
+++ app/src/main/java/com/example/android/lifetrackerlite/GoalRecyclerAdapter.java
@@ -0,0 +1,188 @@
+package com.example.android.lifetrackerlite;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.android.lifetrackerlite.data.LTContract.GoalsHabitsEntry;
+
+import com.example.android.lifetrackerlite.data.LTContract;
+
+import java.text.SimpleDateFormat;
+
+
+//RecyclerView Adapter to populate goals and habits data in app
+public class GoalRecyclerAdapter extends RecyclerView.Adapter<GoalRecyclerAdapter.ViewHolder> {
+    Cursor dataCursor;
+    Context context;
+
+    private static final String TAG = GoalRecyclerAdapter.class.getSimpleName();
+
+    // OnClickListener for items in the recyclerView
+    final private ListItemClickListener mOnClickListener;
+
+    // ListItemClickListener with onListItemClick callback for goalsHabitsFeatureActivity to know which list item was clicked
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView goalName;
+        public TextView goalType;
+        public TextView goalDetails;
+        public TextView streakLengthView;
+        public ImageView goalHabitIcon;
+        public ViewHolder(View view) {
+
+            // Get references to all TextViews,ImageViews within view that need to be populated with data
+            // Set onClickListener on View to provide info about which list item was clicked
+            super(view);
+            goalName = (TextView) view.findViewById(R.id.goal_name);
+            goalType = (TextView) view.findViewById(R.id.goal_type);
+            goalDetails = (TextView) view.findViewById(R.id.goal_details);
+            streakLengthView = (TextView) view.findViewById(R.id.streak_length);
+            goalHabitIcon = (ImageView) view.findViewById(R.id.goal_habit_icon);
+            view.setOnClickListener(this);
+
+        }
+
+
+        @Override
+        public void onClick(View view) {
+
+            // Pass back click position via callback
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+        }
+    }
+
+    public GoalRecyclerAdapter(Activity mContext, Cursor cursor, ListItemClickListener listener) {
+        dataCursor = cursor;
+        context = mContext;
+        mOnClickListener = listener;
+    }
+
+    @Override
+    public GoalRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View goalView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_goalshabits_item, parent, false);
+        return new ViewHolder(goalView);
+    }
+
+    // Swap cursor with loader data if data changes
+    public Cursor swapCursor(Cursor cursor) {
+        if (dataCursor == cursor) {
+            return null;
+        }
+        Cursor oldCursor = dataCursor;
+        this.dataCursor = cursor;
+        if (cursor != null) {
+            this.notifyDataSetChanged();
+        }
+        return oldCursor;
+    }
+
+    // Set cursor goal/habit data on viewHolder
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+
+        dataCursor.moveToPosition(position);
+
+        //Set goal name string
+        String goalNameText = dataCursor.getString(dataCursor.getColumnIndexOrThrow(LTContract.GoalsHabitsEntry.COLUMN_GOAL_NAME));
+        holder.goalName.setText(goalNameText);
+
+        //Set goal type string
+        String goalTypeText = GoalsHabitsEntry.getGoalTypeString(dataCursor.getInt(dataCursor.getColumnIndexOrThrow(GoalsHabitsEntry.COLUMN_GOAL_TYPE)));
+        holder.goalType.setText(goalTypeText);
+
+        //Determine if goal or habit and set the icon accordingly
+        int goalOrHabit = dataCursor.getInt(dataCursor.getColumnIndexOrThrow(GoalsHabitsEntry.COLUMN_GOAL_OR_HABIT));
+        if (goalOrHabit == GoalsHabitsEntry.GOAL) {
+            holder.goalHabitIcon.setImageResource(R.drawable.goal_icon);
+        }
+        if (goalOrHabit == GoalsHabitsEntry.HABIT) {
+            holder.goalHabitIcon.setImageResource(R.drawable.habit_icon);
+        }
+
+        //Get data for goal details string
+
+        //Convert unix start date to string and add to details
+        long startDateMillis = dataCursor.getLong(dataCursor.getColumnIndexOrThrow(GoalsHabitsEntry.COLUMN_GOAL_START_DATE)) * 1000;
+        SimpleDateFormat startSdf = new SimpleDateFormat("MMMM d, yyyy");
+        String startDateString = startSdf.format(startDateMillis);
+
+        //Convert unix end date to string and add to details
+        long endDateMillis = dataCursor.getLong(dataCursor.getColumnIndexOrThrow(GoalsHabitsEntry.COLUMN_GOAL_END_DATE)) * 1000;
+        SimpleDateFormat endSdf = new SimpleDateFormat("MMMM d, yyyy");
+        String endDateString = endSdf.format(endDateMillis);
+
+        //Set goal details string
+        String goalDetailString = "";
+        goalDetailString += "S: " + startDateString + "\n";
+        goalDetailString += "E: " + endDateString;
+        holder.goalDetails.setText(goalDetailString);
+
+        //Get data for streak details string
+        long currentTimeMillis = System.currentTimeMillis();
+        long streakLengthMillis = currentTimeMillis - startDateMillis;
+        long streakLengthDays = streakLengthMillis / (1000 * 60 * 60 * 24);
+        long totalGoalLengthMillis = endDateMillis - startDateMillis;
+        long totalGoalLengthDays = totalGoalLengthMillis / (1000 * 60 * 60 * 24);
+        int streakCompletionPercent = (int) Math.round(((double) streakLengthDays / (double) totalGoalLengthDays) * 100);
+
+        //Set streak details string
+        String streakDetailsString = "";
+        streakDetailsString += Long.toString(streakLengthDays) + " days" + "\n";
+        streakDetailsString += Integer.toString(streakCompletionPercent) + "%";
+        holder.streakLengthView.setText(streakDetailsString);
+
+        //Set color of streak circle based on streak percent completion
+        int streakColor = ContextCompat.getColor(context, getStreakColor(streakCompletionPercent));
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) holder.streakLengthView.getBackground();
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(streakColor);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (dataCursor == null) ? 0 : dataCursor.getCount();
+    }
+
+    public int getStreakColor(int streakCompletionPercent) {
+        int streakColor;
+        if (streakCompletionPercent >= 0 && streakCompletionPercent < 10) {
+            streakColor = R.color.streakLength10;
+        } else if (streakCompletionPercent >= 10 && streakCompletionPercent < 20) {
+            streakColor = R.color.streakLength20;
+        } else if (streakCompletionPercent >= 20 && streakCompletionPercent < 30) {
+            streakColor = R.color.streakLength30;
+        } else if (streakCompletionPercent >= 30 && streakCompletionPercent < 40) {
+            streakColor = R.color.streakLength40;
+        } else if (streakCompletionPercent >= 40 && streakCompletionPercent < 50) {
+            streakColor = R.color.streakLength50;
+        } else if (streakCompletionPercent >= 50 && streakCompletionPercent < 60) {
+            streakColor = R.color.streakLength60;
+        } else if (streakCompletionPercent >= 60 && streakCompletionPercent < 70) {
+            streakColor = R.color.streakLength70;
+        } else if (streakCompletionPercent >= 70 && streakCompletionPercent < 80) {
+            streakColor = R.color.streakLength80;
+        } else if (streakCompletionPercent >= 80 && streakCompletionPercent < 90) {
+            streakColor = R.color.streakLength90;
+        } else {
+            streakColor = R.color.streakLength100;
+        }
+
+        return streakColor;
+    }
+}
--- app/src/main/java/com/example/android/lifetrackerlite/GoalsHabitsFeatureActivity.java
+++ app/src/main/java/com/example/android/lifetrackerlite/GoalsHabitsFeatureActivity.java
@@ -13,6 +13,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
@@ -25,19 +27,19 @@ import com.example.android.lifetrackerlite.GoalEditorActivity;
 
 import java.util.List;
 
-public class GoalsHabitsFeatureActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class GoalsHabitsFeatureActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, GoalRecyclerAdapter.ListItemClickListener{
 
     private static final int GOALSHABITS_LOADER = 0;
 
-    private GoalAdapter mGoalAdapter;
-    private LTDbHelper mLTDbHelper;
-    private SQLiteDatabase mLTDatabase;
+    private RecyclerView mRecyclerView;
+    private GoalRecyclerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals_habits_feature);
 
+        // Add new goal button
         FloatingActionButton addGoalButton = findViewById(R.id.add_goal);
         addGoalButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +54,7 @@ public class GoalsHabitsFeatureActivity extends AppCompatActivity implements Loa
             }
         });
 
+        // Add new habit button
         FloatingActionButton addHabitButton = findViewById(R.id.add_habit);
         addHabitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,25 +69,12 @@ public class GoalsHabitsFeatureActivity extends AppCompatActivity implements Loa
             }
         });
 
-        //Get background listview and set adapter to show goal data
-        ListView goalsHabitsListView = (ListView) findViewById(R.id.list_view_goals_habits);
-        mGoalAdapter = new GoalAdapter(this, null);
-        goalsHabitsListView.setAdapter(mGoalAdapter);
-        goalsHabitsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Intent intent = new Intent(GoalsHabitsFeatureActivity.this, GoalEditorActivity.class);
-
-                // Set the URI on the intent of the goal clicked to be the id of the clicked item
-                Uri currentGoalUri = ContentUris.withAppendedId(GoalsHabitsEntry.CONTENT_URI, id);
-
-                //Set the data on the intent to be the current URI
-                intent.setData(currentGoalUri);
-
-                // Launch the intent to the editor activity to open the activity in "edit mode"
-                startActivity(intent);
-            }
-        });
+        // Find recyclerView for goal/habit list and set linearLayoutManager and recyclerAdapter on recyclerView
+        mRecyclerView = findViewById(R.id.recycler_view_goals_habits);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(GoalsHabitsFeatureActivity.this));
+        mAdapter = new GoalRecyclerAdapter(this, null, this);
+        mRecyclerView.setAdapter(mAdapter);
 
         //Initialize loader for goals data
         getLoaderManager().initLoader(GOALSHABITS_LOADER, null, this);
@@ -112,14 +102,33 @@ public class GoalsHabitsFeatureActivity extends AppCompatActivity implements Loa
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
 
-        mGoalAdapter.swapCursor(cursor);
+        mAdapter.swapCursor(cursor);
 
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
-        mGoalAdapter.swapCursor(null);
+        mAdapter.swapCursor(null);
+
+    }
+
+    // When a goal/habit item is clicked, open up that goal/habit in "edit mode"
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
+        // New intent to open GoalEditorActivity class for goal clicked
+        Intent intent = new Intent(GoalsHabitsFeatureActivity.this, GoalEditorActivity.class);
+
+        // Set the URI on the intent of the goal clicked to be the id of the clicked item
+        // Add 1 to the clickedItemIndex so that URI ID matches correct goal
+        Uri currentGoalUri = ContentUris.withAppendedId(GoalsHabitsEntry.CONTENT_URI, clickedItemIndex + 1);
+
+        //Set the data on the intent to be the current URI
+        intent.setData(currentGoalUri);
+
+        // Launch the intent to the editor activity to open the activity in "edit mode"
+        startActivity(intent);
 
     }
 }
